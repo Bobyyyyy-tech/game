cceb16d2a5bf5daa85af9cebe2f1bc2bbc097306
Bobyyyyy-tech <Mabeluke1234@gmail.com>
2026-03-25 20:57:49 +1100

    Implement terrain generation and block updates
    Added world generation and block update functionality.

--- server.js
+++ server.js
@@ -9,17 +9,29 @@ const io = new Server(server)
 app.use(express.static("public"))
 
 let players = {}
+let world = {} // key = "x,y,z" -> block type
+
+function key(x,y,z){ return x+","+y+","+z }
+
+// generate simple terrain once
+for(let x=-20;x<20;x++){
+    for(let z=-20;z<20;z++){
+        let h = Math.floor(Math.sin(x*0.2)*2 + Math.cos(z*0.2)*2 + 5)
+        for(let y=0;y<h;y++){
+            world[key(x,y,z)] = y===h-1 ? 1 : 2 // 1 grass, 2 dirt
+        }
+    }
+}
 
 io.on("connection", socket => {
 
-    players[socket.id] = {
-        x: 0,
-        y: 1.8,
-        z: 0,
-        name: "Player_" + Math.floor(Math.random()*1000)
-    }
+    players[socket.id] = { x:0, y:10, z:0 }
 
-    socket.emit("init", { id: socket.id, players })
+    socket.emit("init", {
+        id: socket.id,
+        players,
+        world
+    })
 
     socket.broadcast.emit("playerJoined", {
         id: socket.id,
@@ -27,16 +39,23 @@ io.on("connection", socket => {
     })
 
     socket.on("move", data => {
-        if (!players[socket.id]) return
-
-        players[socket.id].x = data.x
-        players[socket.id].y = data.y
-        players[socket.id].z = data.z
-
+        players[socket.id] = data
         socket.broadcast.emit("playerMoved", {
             id: socket.id,
-            ...players[socket.id]
+            ...data
         })
+    })
+
+    socket.on("blockUpdate", data => {
+        const k = key(data.x,data.y,data.z)
+
+        if(data.type === 0){
+            delete world[k]
+        } else {
+            world[k] = data.type
+        }
+
+        io.emit("blockUpdate", data)
     })
 
     socket.on("disconnect", () => {
@@ -46,4 +65,4 @@ io.on("connection", socket => {
 })
 
 const PORT = process.env.PORT || 3000
-server.listen(PORT, () => console.log("Running on " + PORT))
+server.listen(PORT, () => console.log("Running " + PORT))
